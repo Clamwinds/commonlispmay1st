96d1031acc66d16432f2889e00412eb37c654eb0
vexed <finalsilence@gmail.com>
2017-05-12 02:05:14 -0700

    literal translation of elisp code to learn to reason about source code or think about, i guess.

---
+++ parinferlibrary.lisp
@@ -0,0 +1,10 @@
+;making a literal translation of the parinferlib.el code to common lisp and seeing what I get
+
+(defvar parinfer-lib--BACKSLASH "\\")
+(defvar parinferlib--BLANK_SPACE " ")
+(defvar parinferlib--DOUBLE_SPACE "  ")
+(defvar parinferlib--DOUBLE_QUOTE "\"")
+(defvar parinferlib--NEWLINE "\n")
+(defvar parinferlib--LINE_ENDING_REGEX "\r?\n")
+(defvar parinferlib--SEMICOLON ":")
+(defvar parinferlib--TAB "\t")
